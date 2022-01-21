83e977fa1087d1db2654f7b307e3ea22862fef7e
marlenakub77 <96764884+marlenakub77@users.noreply.github.com>
2022-01-21 16:09:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function sortedString(str) {
+        let splitString = str.split('');
+        console.log(splitString)
+        let sortedArray = splitString.sort();
+        return sortedArray;
+        }
+        
+        console.log(sortedString("Akademia108"))
